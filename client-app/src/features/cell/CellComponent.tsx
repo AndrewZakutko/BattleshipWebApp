cd917a7e57d8bec7707e27b22da5c59c585eb44c
Andrew Zakutko <zakutkoandrey@gmail.com>
2022-04-30 11:47:41 +0300

    Added changes

---
+++ client-app/src/features/cell/CellComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+    x: number;
+    y: number,
+    cellStatus: string;
+}
+
+export default function CellComponent() {
+  return (
+    <th></th>
+  )
+}
